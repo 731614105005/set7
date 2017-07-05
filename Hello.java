a3d509d798f6eb67a3b94206aea11a1b03358b6a
731614105005 <bawadharani96@gmail.com>
2017-07-05 09:38:56 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+# set7
+import java.io.*;
+ import java.util.*;
+ public class Hello
+ {
+   public static void main(String args[])throws IOException
+   {
+    Scanner sc=new Scanner(System.in);
+     char b;
+     int i,n=5;
+     for(i=0;i<n;i++)
+     {
+       System.out.println("Hello");
+       }
+       }
+       }
